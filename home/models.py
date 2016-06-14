d8bc3f9b907d735c86a7b08f18e608b200b08da6
Stefan Schärmeli <schaermu@gmail.com>
2016-06-14 13:32:58 +0200

    added StreamField for home

--- home/models.py
+++ home/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailcore import blocks
+from wagtail.wagtailadmin.edit_handlers import (FieldPanel, MultiFieldPanel,
+                                                InlinePanel, StreamFieldPanel)
+from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 from projects.models import ProjectPage
 
@@ -12,10 +16,16 @@ from projects.models import ProjectPage
 class HomePage(Page):
     heading = models.CharField(max_length=100, default='')
     lead = models.CharField(max_length=150, default='')
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
 
     content_panels = Page.content_panels + [
         FieldPanel('heading'),
-        FieldPanel('lead')
+        FieldPanel('lead'),
+        StreamFieldPanel('body'),
     ]
 
     def get_context(self, request):
