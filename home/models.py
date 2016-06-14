7e1cb270e3d6a2a691b8b9df9006ef87f5342329
Stefan Schärmeli <schaermu@gmail.com>
2016-06-14 12:15:40 +0200

    using hydrogen as a template, added projects app

--- home/models.py
+++ home/models.py
@@ -2,14 +2,25 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
+
+from projects.models import ProjectPage
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    heading = models.CharField(max_length=100, default='')
+    lead = models.CharField(max_length=150, default='')
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full")
+        FieldPanel('heading'),
+        FieldPanel('lead')
     ]
+
+    def get_context(self, request):
+        context = super(HomePage, self).get_context(request)
+
+        # Add extra variables and return the updated context
+        context['projects'] = ProjectPage.objects.child_of(self).live()
+        return context
