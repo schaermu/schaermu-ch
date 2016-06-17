9455d59044f0e52d6648580a00ef0abbe3c5075b
Stefan Schärmeli <schaermu@gmail.com>
2016-06-17 13:32:33 +0200

    mailgun config

--- schaermu_ch/settings/base.py
+++ schaermu_ch/settings/base.py
@@ -18,7 +18,6 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -32,6 +31,7 @@ INSTALLED_APPS = [
     'search',
     'common',
     'bootstrap3',
+    'anymail',
 
     'wagtail.wagtailforms',
     'wagtail.wagtailredirects',
