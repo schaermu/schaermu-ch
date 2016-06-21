f94da6a961795addcdfbd1eaa437145f3c010c98
Stefan Schärmeli <schaermu@gmail.com>
2016-06-21 15:14:40 +0200

    added export script, removed test export

---
+++ export.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+DJANGO_SETTINGS_MODULE=schaermu_ch.settings.dev django-admin.py dumpdata --natural-foreign --indent=4 -e contenttypes -e auth.Permission -e sessions -e wagtailcore.pagerevision > export.json
