87ed7e29573c312849b44ab956565b0b518a4923
Stefan Schärmeli <schaermu@gmail.com>
2023-06-12 20:19:52 +0200

    feat: svelte integration

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let messages = [];
+    let greeting = ''
+
+    function handleGreetClick()  {
+        greeting = messages[Math.floor(Math.random() * messages.length)];
+    }
+    
+    handleGreetClick();
+</script>
+
+<div> 
+    <h3>{greeting}! Thank you for visiting!</h3>
+    <button on:click={handleGreetClick}>
+        New Greeting
+    </button>
+</div>
