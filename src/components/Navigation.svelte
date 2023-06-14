c6385823e6787fd6cf92c1211e40093ea250cfcb
Stefan Schärmeli <schaermu@gmail.com>
2023-06-14 08:04:22 +0200

    feat: refactor navigation into svelte comp

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,18 @@
+<script>
+
+let expanded = false;
+
+</script>
+
+<div class="hamburger" on:click={() => expanded = !expanded}>
+    <span class="line"></span>
+    <span class="line"></span>
+    <span class="line"></span>
+</div>
+
+<div class="nav-links" class:expanded="{expanded}">
+    <a href="/">Home</a>
+    <a href="/about/">About</a>
+    <a href="/blog/">Blog</a>
+    <a href="/tags/">Tags</a>
+</div>
