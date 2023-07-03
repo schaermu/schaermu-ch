f96f7eb8e84ed907f554fe2d5405860b48aa538b
Stefan Schärmeli <info@schaermu.ch>
2023-07-03 15:37:34 +0200

    fix: e2e test, imports

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -2,5 +2,5 @@ it('titles are correct', () => {
     cy.visit('/');
 
     cy.title().should('eq', 'the writings and musings of schaermu.');
-    cy.get('h1').should('have.text', 'Welcome!');
+    cy.get('h1').should('contain.text', 'Willkommen!');
 });
