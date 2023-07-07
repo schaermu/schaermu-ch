ee96e60ef09161af720521953fb42cff28cbd2bc
Stefan Schärmeli <info@schaermu.ch>
2023-07-07 12:04:52 +0200

    test: adds e2e test for home page

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,6 +1,18 @@
-it('titles are correct', () => {
-    cy.visit('/');
+describe('home page', () => {
+    it('has correct titles', () => {
+        cy.visit('/');
 
-    cy.title().should('eq', 'der bloggende schärmu');
-    cy.get('h1').should('contain.text', 'Willkommen!');
-});
+        cy.title().should('eq', 'der bloggende schärmu');
+        cy.get('h1').should('contain.text', 'Willkommen!');
+    });
+
+    it('contains 2 cards linking to blog and cv', () => {
+        cy.visit('/');
+
+        cy.get('article').should('have.length', 2);
+        cy.get('article').each(($el) => {
+            cy.wrap($el).get('a').should('exist');
+            cy.wrap($el).get('a').should('have.attr', 'href');
+        })
+    });
+})
