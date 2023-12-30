ea9a6e84c7576bd02bab4ebe4f7fe09e80d55f16
Stefan Schärmeli <info@schaermu.ch>
2023-12-30 19:30:17 +0100

    fix: tests for home amended

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -3,17 +3,6 @@ import { test, expect } from '@playwright/test';
 test('has correct titles', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page).toHaveTitle(/der begrüssende schärmu/);
-  await expect(page.locator('h1')).toHaveText('Willkommen!');
-});
-
-test('contains 2 cards linking to blog and cv', async ({ page }) => {
-  await page.goto('/');
-  const articles = await page.locator('article');
-
-  await expect(articles).toHaveCount(2);
-
-  for (const article of await page.locator('article').all()) {
-    await expect(article.locator('a').nth(0)).toHaveAttribute('href', /(blog|cv)/)
-  }
+  await expect(page).toHaveTitle(/der bloggende schärmu/);
+  await expect(page.locator('h1')).toHaveText('Schärmu\'s Blog');
 });
