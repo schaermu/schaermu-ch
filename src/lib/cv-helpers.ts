8f994e2d3c040d3e79244795aa3b07dd67022f8b
Stefan Schärmeli <info@schaermu.ch>
2024-04-20 07:04:03 +0000

    adds skill tooltips

--- src/lib/cv-helpers.ts
+++ src/lib/cv-helpers.ts
@@ -13,7 +13,7 @@ const SKILL_MODIFIERS = [
 // aggregate tech skills into groups
 const SKILL_GROUPS = [
     { label: "Frontend", skills: ["HTML/CSS", "JavaScript", "Angular"] },
-    { label: "Backend", skills: ["Node.JS", "Dotnet", "Python", "Django", "PHP"] },
+    { label: "Backend", skills: ["Golang", "Node.JS", "Dotnet", "Python", "Django", "PHP"] },
     { label: "Cloud", skills: ["AWS", "Cloud-Migration", "Azure", "GCP"] },
     { label: "Container", skills: ["Docker", "Kubernetes", "Helm"] }
 ]
@@ -21,6 +21,14 @@ const SKILL_GROUPS = [
 // define skill groups
 const TECH_SKILLS = ["Frontend", "Backend", "Cloud", "Container", "Security", "Automatisierung"]
 const METHOD_SKILLS = ["Agile", "SAFe", "Requirements Management", "Design Thinking", "Scrum", "Kanban", "Design Thinking"]
+
+const getToolTip = (label: string) => {
+    const idx = SKILL_GROUPS.findIndex(group => group.label === label);
+    if (idx > -1) {
+        return SKILL_GROUPS[idx].skills.join(", ");
+    }
+    return label;
+}
 
 const getMaxDays = (entries: CollectionEntry<"cv-entries">[]) => {
     const maxFromDate = entries.map(entry => entry.data.from).reduce((acc: Date, date: Date) => {
@@ -109,4 +117,5 @@ const calculateSkillScore = (entry: CollectionEntry<"cv-entries">, maxDays: numb
 
 export {
     getTopSkills,
+    getToolTip
 }
