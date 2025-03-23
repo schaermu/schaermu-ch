823ce267fb74e5895f1a4e3d5d351fc862d99c6d
Stefan Schärmeli <519494+schaermu@users.noreply.github.com>
2025-03-23 11:57:41 +0100

    moving more config

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? [['github'], ['html']] : [['list']],
+  reporter: process.env.CI ? [
+    ['github'], ['html', { open: 'never', outputFolder: '/app/playwright-reports' }]
+  ] : [['list']],
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
